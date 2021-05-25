0d3ef68b50b950caa962d79a388d3edc7b3893f7
lapape <lapape07@gmail.com>
2021-05-24 22:49:45 -0600

    added menu function and getAllEmployees function that displays all employees in a table

--- schema.sql
+++ schema.sql
@@ -23,3 +23,11 @@ CREATE TABLE employee(
   manager_id INTEGER,
   PRIMARY KEY (id)
 );
+INSERT INTO department (name)
+VALUES ("Legal"),("Finance"),("IT"),("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 100000.00, 1),("Paralegal", 50000.00, 1),("Accountant", 50000.00, 2),("Secratary", 45000.00, 2),("Tech Support", 100000.00, 3),("Sales Rep", 60000.00, 4),("Sales Intern", 45000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Becky", "Smith", 1, 1),("Jane", "Doe", 1, 1),("Joe", "Young", 2, 1),("Sarah", "Secratary", 4, 2),("Jordy", "LaForge", 5, 3),("Beverly", "Crusher",6, 4)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const Choice = require("inquirer/lib/objects/choice");
 require("dotenv").config();
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -13,8 +14,50 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+function menu() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add employee",
+        "Remove employee",
+        "Update employee role",
+        "Update employee manager",
+        "exit",
+      ],
+      name: "choice",
+    })
+    .then((res) => {
+      switch (res.choice) {
+        case "View all employees": {
+          getAllEmployees();
+          break;
+        }
+
+        case "exit": {
+          connection.end();
+          break;
+        }
+      }
+    });
+}
+
+const getAllEmployees = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    menu();
+  });
+};
+
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}\n`);
-  //   init();
+  menu();
 });
