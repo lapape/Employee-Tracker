83bfcb6a7b2e45cd43cd2cc33b6df1a3ae73b9ee
lapape <lapape07@gmail.com>
2021-05-25 21:00:02 -0600

    started the addEmployee function

--- schema.sql
+++ schema.sql
@@ -3,20 +3,20 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-  id INTEGER AUTO_INCREMENT NOT NULL,
+  id INTEGER AUTO_INCREMENT,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-  id INTEGER AUTO_INCREMENT NOT NULL,
+  id INTEGER AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL(10,2),
   department_id INTEGER,
   PRIMARY KEY (id)
 );
 CREATE TABLE employee(
-  id INTEGER AUTO_INCREMENT NOT NULL,
+  id INTEGER AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER,
@@ -24,10 +24,13 @@ CREATE TABLE employee(
   PRIMARY KEY (id)
 );
 INSERT INTO department (name)
-VALUES ("Legal"),("Finance"),("IT"),("Sales");
+VALUES ("Legal"),("Finance"),("IT"),("Sales"),("Management");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 100000.00, 1),("Paralegal", 50000.00, 1),("Accountant", 50000.00, 2),("Secratary", 45000.00, 2),("Tech Support", 100000.00, 3),("Sales Rep", 60000.00, 4),("Sales Intern", 45000.00, 4);
+VALUES ("Lawyer", 100000.00, 1),("Paralegal", 50000.00, 1),("Accountant", 50000.00, 2),("Secratary", 45000.00, 2),("Tech Support", 100000.00, 3),("Sales Rep", 60000.00, 4),("Sales Intern", 45000.00, 4),("Manager", 700000, 5);
 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Jean Luc", "Picard", 8),("Big", "Boss", 8),("Donky", "Kong", 8),("Jenny", "Lopez", 8);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Becky", "Smith", 1, 1),("Jane", "Doe", 1, 1),("Joe", "Young", 2, 1),("Peter", "Parker", 3, 2),("Sarah", "Secratary", 4, 2),("Jordy", "LaForge", 5, 3),("Beverly", "Crusher",6, 4),("Wesley", "Crusher", 7, 4)
+VALUES ("Becky", "Smith", 1, 10),("Jane", "Doe", 1, 10),("Joe", "Young", 2, 11),("Peter", "Parker", 3, 12),("Sarah", "Secratary", 4, 12),("Jordy", "LaForge", 5, 9),("Beverly", "Crusher",6, 9),("Wesley", "Crusher", 7, 9);
+
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const Choice = require("inquirer/lib/objects/choice");
 require("dotenv").config();
 const cTable = require("console.table");
 
+const managerArray = [];
+
 const connection = mysql.createConnection({
   host: "localhost",
-
   port: 3306,
-
   user: process.env.DB_USER,
-
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
@@ -23,7 +22,8 @@ function menu() {
       choices: [
         "View all employees",
         "View all employees by department",
-        "View all employees by manager",
+        "View all employees by role",
+        // "View all employees by manager",
         "Add employee",
         "Remove employee",
         "Update employee role",
@@ -40,6 +40,30 @@ function menu() {
         }
         case "View all employees by department": {
           getDepartment();
+          break;
+        }
+        case "View all employees by role": {
+          getRole();
+          break;
+        }
+        // case "View all employees by manager": {
+        //   getManager();
+        //   break;
+        // }
+        case "Add employee": {
+          getManagers();
+          break;
+        }
+        case "Remove employee": {
+          getManagers();
+          break;
+        }
+        case "Update employee role": {
+          getManagers();
+          break;
+        }
+        case "Update employee manager": {
+          getManagers();
           break;
         }
 
@@ -70,7 +94,7 @@ const getDepartment = () => {
     .prompt({
       type: "list",
       message: "Which department?",
-      choices: ["Legal", "Finance", "IT", "Sales"],
+      choices: ["Legal", "Finance", "IT", "Sales", "Management"],
       name: "departments",
     })
     .then((res) => {
@@ -91,7 +115,7 @@ const getDepartment = () => {
         }
         case "Finance": {
           connection.query(
-            //select all employees where role id is 1 or 2 which are members of the Legal department
+            //select all employees where role id is 2 or 3 which are members of the Finance department
             "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 3 OR role_id = 4",
             (err, res) => {
               if (err) throw err;
@@ -105,7 +129,7 @@ const getDepartment = () => {
         }
         case "IT": {
           connection.query(
-            //select all employees where role id is 1 or 2 which are members of the Legal department
+            //select all employees where role id is 5 which are members of the IT department
             "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 5",
             (err, res) => {
               if (err) throw err;
@@ -119,7 +143,7 @@ const getDepartment = () => {
         }
         case "Sales": {
           connection.query(
-            //select all employees where role id is 1 or 2 which are members of the Legal department
+            //select all employees where role id is 6 or 7 which are members of the Sales department
             "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 6 OR role_id = 7",
             (err, res) => {
               if (err) throw err;
@@ -131,7 +155,222 @@ const getDepartment = () => {
           );
           break;
         }
+        case "Management": {
+          connection.query(
+            //select all employees where role id is 8 which are members of the Management department
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 8",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
       }
+    });
+};
+
+const getRole = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Which role?",
+      choices: [
+        "Lawyer",
+        "Paralegal",
+        "Accountant",
+        "Secratary",
+        "Tech Support",
+        "Sales Rep",
+        "Sales Intern",
+        "Manager",
+      ],
+      name: "roles",
+    })
+    .then((res) => {
+      switch (res.roles) {
+        case "Lawyer": {
+          connection.query(
+            //select all employees where role id is 1 for Lawyer
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 1",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Paralegal": {
+          connection.query(
+            //select all employees where role id is 2 for Paralegal
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 2",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Accountant": {
+          connection.query(
+            //select all employees where role id is 3 for Accountant
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 3",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Secratary": {
+          connection.query(
+            //select all employees where role id is 4 for Secratary
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 4",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Tech Support": {
+          connection.query(
+            //select all employees where role id is 5 for Tech Support
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 5",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Sales Rep": {
+          connection.query(
+            //select all employees where role id is 6 for Sales Rep
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 6",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Sales Intern": {
+          connection.query(
+            //select all employees where role id is 7 for Sales Intern
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 7",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+        case "Manager": {
+          connection.query(
+            //select all employees where role id is 8 for Manager
+            "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id WHERE role_id = 8",
+            (err, res) => {
+              if (err) throw err;
+              // Log all results of the SELECT statement in a table
+              console.table(res);
+              //returns user to main menu
+              menu();
+            }
+          );
+          break;
+        }
+      }
+    });
+};
+
+function getManagers() {
+  connection.query("SELECT * FROM employee WHERE role_id = 8", (err, res) => {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      managerArray.push(res[i].first_name);
+    }
+    console.log(managerArray);
+    // Log all results of the SELECT statement
+    addEmployee(managerArray);
+  });
+}
+
+const addEmployee = (managerArray) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Employee's first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the Employee's last name?",
+        name: "lastName",
+      },
+      {
+        type: "rawlist",
+        message: "What is the Employee's role?",
+        name: "role",
+        choices: [
+          "Lawyer",
+          "Paralegal",
+          "Accountant",
+          "Secratary",
+          "Tech Support",
+          "Sales Rep",
+          "Sales Intern",
+          "Manager",
+        ],
+      },
+      {
+        type: "rawlist",
+        message: "Who is the Employee's Manager?",
+        name: "manager",
+        choices: managerArray,
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      const query = connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: res.firstName,
+          last_name: res.lastName,
+          role_id: Number(res.role),
+          manager_id: Number(res.manager),
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(res);
+          menu();
+        }
+      );
     });
 };
 
